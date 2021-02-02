a6d804cc48ad055d90d784f8821bbb902f580642
arkon36 <alec.b.hamilton@hotmail.com>
2021-02-01 23:33:10 -0500

    Code de Base pour l'affichage.
    La code matlab reste a etre fait et le code pour charger les fichier .m de matlab a partir de l'application qui affiche reste aussi à être fait

---
+++ Affichage/Affichage/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Affichage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainWindowModel model = new MainWindowModel();
+            this.DataContext = model;
+            model.listeBattement.Insert(0, new BattementCardiaque("5:35", 70));
+            model.listeBattement.Insert(0, new BattementCardiaque("5:36", 73));
+            model.listeBattement.Insert(0, new BattementCardiaque("5:37", 65));
+            model.listeBattement.Insert(0, new BattementCardiaque("5:38", 68));
+            model.LastBattement = model.listeBattement.First().Battement;
+        }
+
+
+        /*********
+         * Trouver une facon d'ajouter matlab
+         * https://www.mathworks.com/matlabcentral/answers/705763-why-am-i-getting-the-error-the-type-or-namespace-mlapp-could-not-be-found-when-building-c-appli
+         * */
+        static void LoadMatlab(string[] args)
+        {
+
+
+            /*
+            // Create the MATLAB instance 
+            MLApp.MLApp matlab = new MLApp.MLApp();
+
+            // Change to the directory where the function is located 
+            matlab.Execute(@"cd c:\temp\example");
+
+            // Define the output 
+            object result = null;
+
+            // Call the MATLAB function myfunc
+            matlab.Feval("myfunc", 2, out result, 3.14, 42.0, "world");
+
+            // Display result 
+            object[] res = result as object[];
+
+            */
+        }
+    }
+}
---
+++ Affichage/Affichage/MainWindowModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Affichage
+{
+    class MainWindowModel : INotifyPropertyChanged
+    {
+
+
+        public BindingList<BattementCardiaque> listeBattement { get; set; }
+        public void AddItemToList(string _temps, int _battement)
+        {
+            listeBattement.Add(new BattementCardiaque(_temps, _battement));
+        }
+
+        private int lastBattement;
+        public int LastBattement
+        {
+            get
+            {
+                return lastBattement;
+            }
+            set
+            {
+                lastBattement = value;
+                OnPropertyChanged("LastBattement");
+            }
+        }
+
+        public MainWindowModel()
+        {
+            listeBattement = new BindingList<BattementCardiaque>();
+        }
+
+        #region INotifyPropertyChanged Members  
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+
+
+    class BattementCardiaque : INotifyPropertyChanged
+    {
+
+        private int battement;
+        private string temps;
+        public int Battement
+        {
+            get
+            {
+                return battement;
+            }
+            set
+            {
+                battement = value;
+                OnPropertyChanged("Battement");
+            }
+        }
+        public string Temps
+        {
+            get
+            {
+                return temps;
+            }
+            set
+            {
+                temps = value;
+                OnPropertyChanged("Temps");
+            }
+        }
+
+        public BattementCardiaque(string _temps, int _battement)
+        {
+            Temps = _temps;
+            Battement = _battement;
+        }
+
+
+
+        #region INotifyPropertyChanged Members  
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
